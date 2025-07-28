29fa3918492d6a8dc8b1e28a04d07659584ee738
Aaron Beavers <foamy125@gmail.com>
2025-07-28 11:43:48 -0500

    vault backup: 2025-07-28 11:43:48

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,27 @@ module.exports = {
         {
             name: "stt_service",
             script: "./services/stt/run.sh",
+
+            cwd:"./services/stt",
             interpreter: "bash",
             env_file: ".env",
-            autorestart: true
+            autorestart: true,
+            // Very important until I figure out how to disable the console from popping up every time it restarts.
+            // Makes my computer unusable if the program is in a bad state while developing
+            restart_delay: 10000,
+            kill_timeout: 10000
         },
         {
             name: "tts_service",
             script: "./services/tts/run.sh",
+            cwd:"./services/tts/",
             interpreter: "bash",
             env_file: ".env",
-            autorestart: true
+            autorestart: true,
+            // Very important until I figure out how to disable the console from popping up every time it restarts.
+            // Makes my computer unusable if the program is in a bad state while developing
+            restart_delay: 10000,
+            kill_timeout: 10000
         }
     ]
 };
